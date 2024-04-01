6b6cd75f9b4640928b0631d41b5db5441eeda469
augustine-awuori <codewithaugustine@gmail.com>
2024-04-01 11:51:14 +0300

    Only show the cart when it has products

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -56,31 +56,35 @@ function Header() {
             </Button>
           </Link>
           <Box>
-            <Link to={"/cart"}>
-              <Icon
-                as={FaShoppingCart}
-                color={"white"}
-                px={2}
-                h={"auto"}
-                w={"auto"}
-                boxSize={10}
-                _hover={{ cursor: "pointer" }}
-              />
-            </Link>
-            <Text
-              bg={"blue.300"}
-              color={"white"}
-              w={6}
-              h={6}
-              align={"center"}
-              border={14}
-              borderRadius={18}
-              position={"fixed"}
-              mt={"-14"}
-              ml={"6"}
-            >
-              {cart.count}
-            </Text>
+            {cart.count ? (
+              <>
+                <Link to={"/cart"}>
+                  <Icon
+                    as={FaShoppingCart}
+                    color={"white"}
+                    px={2}
+                    h={"auto"}
+                    w={"auto"}
+                    boxSize={10}
+                    _hover={{ cursor: "pointer" }}
+                  />
+                </Link>
+                <Text
+                  bg={"blue.300"}
+                  color={"white"}
+                  w={6}
+                  h={6}
+                  align={"center"}
+                  border={14}
+                  borderRadius={18}
+                  position={"fixed"}
+                  mt={"-14"}
+                  ml={"6"}
+                >
+                  {cart.count}
+                </Text>
+              </>
+            ) : null}
           </Box>
         </Box>
       </Flex>
