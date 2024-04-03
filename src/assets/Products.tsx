e63e24afde095aeeb0eca910f5e9dc6b7bd0198f
augustine-awuori <codewithaugustine@gmail.com>
2024-04-03 12:52:55 +0300

    Fix build issue

--- src/assets/Products.tsx
+++ src/assets/Products.tsx
@@ -1,47 +1,45 @@
-import { Product } from "../components/ProductCard";
-import productImage from "../assets/product.jpg"
+import { Product } from "../components/interfaces/Product";
+import productImage from "../assets/product.jpg";
 
 const products: Product[] = [
   {
-    _id: 1,
+    _id: "1",
     description: "A premium product with profound results in a global scale!!",
     name: "Shower Gel",
     image: productImage,
     price: 200,
   },
   {
-    _id: 2,
+    _id: "2",
     description: "A premium product with profound results in a global scale!!",
     name: "Shower Gel",
     image: productImage,
     price: 200,
   },
   {
-    _id: 3,
+    _id: "3",
     description: "A premium product with profound results in a global scale!!",
     name: "Shower Gel",
     image: productImage,
     price: 200,
   },
   {
-    _id: 4,
+    _id: "4",
     description: "A premium product with profound results in a global scale!!",
     name: "Shower Gel",
     image: productImage,
     price: 200,
   },
   {
-    _id: 5,
+    _id: "5",
     description: "A premium product with profound results in a global scale!!",
     name: "Shower Gel",
     image: productImage,
     price: 200,
   },
 ];
-const Products = () => { 
-  return (
-    products
-  )
-}
+const Products = () => {
+  return products;
+};
 
-export default Products
+export default Products;
